f4b02b7ced2ab9d3b75ead2aa83fa8dc0c8a505e
Ninnjah <ninnjah.mail@gmail.com>
2021-11-15 16:53:02 +0300

    Fix grammar

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import random
 def lang() -> None:
     """Language config"""
     try:
-        lang_chois: int = int(input("Choose your language: [1]=EN [2]=RU:"))
+        lang_choice: int = int(input("Choose your language: [1]=EN [2]=RU:"))
     except ValueError:
         print("Choose your language: [1]=EN [2]=RU")
     else:
-        if lang_chois == 1:
+        if lang_choice == 1:
             pass
         else:
             pass
@@ -16,7 +16,7 @@ def lang() -> None:
 
 def err() -> None:
     """Check for win"""
-    if chois == random.randint(1, 3):
+    if choice == random.randint(1, 3):
         print("Congratulations! You won!")
     else:
         print("Sorry, you didn't guess. Try again!")
@@ -32,11 +32,11 @@ if __name__ == "__main__":
     while True:
         try:
             # Check input for integer from 1 to 3
-            chois: int = int(input("Please enter the thimble number (from 1 to 3):"))
+            choice: int = int(input("Please enter the thimble number (from 1 to 3):"))
         except ValueError:
             print("You need to enter a number from 1 to 3")
         else:
-            if 1 <= chois <= 3:
+            if 1 <= choice <= 3:
                 # Check for win
                 err()
             else:
