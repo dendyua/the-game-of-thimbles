841a28114fb96f97957c8cd7363f969bbc1b5b87
pydendy <67948953+dendyua@users.noreply.github.com>
2022-05-03 12:12:53 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 import random
-
 import i18n
 
 
 def lang() -> None:
     """Language config"""
     # set localisations directory
-    i18n.load_path.append("translations/")
+    i18n.load_path.append('translations/')
     # Default localisation
     i18n.set('fallback', 'en-us')
     try:
-        lang_choice: int = int(input("Choose your language: [1]=EN [2]=RU:"))
+        lang_choice: int = int(input('Choose your language: [1]=EN [2]=UA [3]=ru:'))
     except ValueError:
-        print("Choose your language: [1]=EN [2]=RU")
+        print('Choose your language: [1]=EN [2]=UA [3]=ru:')
     else:
-        if lang_choice == 1:
-            pass
+        if lang_choice == 2:
+            i18n.set('fallback', 'ua-ua')
+        elif lang_choice == 3:
+            i18n.set('fallback', 'ru-ru')
         else:
             pass
 
@@ -23,27 +24,27 @@ def lang() -> None:
 def err() -> None:
     """Check for win"""
     if choice == random.randint(1, 3):
-        print(i18n.t("locale.win"))
+        print(i18n.t('locale.win'))
     else:
-        print(i18n.t("locale.lose"))
+        print(i18n.t('locale.lose'))
 
 
 if __name__ == "__main__":
     # Language set
     lang()
 
-    print(i18n.t("locale.welcome"))
+    print(i18n.t('locale.welcome'))
 
     # Main loop
     while True:
         try:
             # Check input for integer from 1 to 3
-            choice: int = int(input(i18n.t("locale.thimble_number")))
+            choice: int = int(input(i18n.t('locale.thimble_number')))
         except ValueError:
-            print(i18n.t("locale.thimble_number_error"))
+            print(i18n.t('locale.thimble_number_error'))
         else:
             if 1 <= choice <= 3:
                 # Check for win
                 err()
             else:
-                print(i18n.t("locale.thimble_number_error"))
+                print(i18n.t('locale.thimble_number_error'))
